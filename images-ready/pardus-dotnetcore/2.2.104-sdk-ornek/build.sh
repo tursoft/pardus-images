e7c07bb4d2848a401f3dcc2005e464521d0e34bf
Muhammet TURSAK <muhammett@datasel.com.tr>
2019-03-01 16:41:13 +0300

    düzenlemeler

---
+++ images-ready/pardus-dotnetcore/2.2.104-sdk-ornek/build.sh
@@ -0,0 +1,2 @@
+sudo docker build . --tag tursoft/pardus-dotnetcore-ornek:2.2.104-sdk
+
--- images-ready/pardus-dotnetcore/2.2.104-sdk/build.sh
+++ images-ready/pardus-dotnetcore/2.2.104-sdk/build.sh
@@ -1,2 +1,1 @@
-sudo docker build -f dockerfile-2.2.104-sdk . --tag tursoft/pardus-dotnetcore:2.2.104-sdk
-sudo docker build -f dockerfile-2.2.104-sdk . --tag tursoft/pardus-dotnetcore:2.2.104-sdk
+sudo docker build . --tag tursoft/pardus-dotnetcore:2.2.104-sdk
