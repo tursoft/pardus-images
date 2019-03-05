1200bde76e6034bf5e3b19c869d21369940dea63
Muhammet TURSAK <muhammett@datasel.com.tr>
2019-03-05 11:53:31 +0300

    openjdk-ornek guncellemesi

--- images-ready/pardus-openjdk/8u181-jdk-ornek/src/Hello.java
+++ images-ready/pardus-openjdk/8u181-jdk-ornek/src/Hello.java
@@ -2,6 +2,11 @@ class Hello
 {  
 	public static void main(String[] args)
 	{	
-		System.out.println("[Pardus.Images] Örnek Java uygulamasından merhabalar.\n\nPardus.Images proje grubu github adresi: https://github.com/tursoft/pardus-images");  
+		System.out.println("=====================================================================================");  
+		System.out.println("[Pardus.Images]");
+		System.out.println("=====================================================================================");  
+		System.out.println("Örnek Java uygulamasından merhabalar....");
+		System.out.println("Pardus.Images proje grubu github adresi: https://github.com/tursoft/pardus-images");  
+		System.out.println("=====================================================================================");  		
 	}  
 }  
